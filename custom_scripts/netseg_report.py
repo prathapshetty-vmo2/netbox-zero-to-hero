3bcdcdefc3392395eb169880c135fdcf86b65e0d
prathapshetty-vmo2 <prathap.chandrashetty@virginmediao2.co.uk>
2025-06-10 10:15:26 +0100

    Create netseg_report.py

---
+++ custom_scripts/netseg_report.py
@@ -0,0 +1,13 @@
+from extras.reports import Report
+from dcim.models import Device
+from utilities.testing import TestCase
+
+class DeviceReport(Report):
+    description = "Check that all devices have a primary IP assigned"
+
+    def test_primary_ip(self):
+        for device in Device.objects.all():
+            if device.primary_ip is None:
+                self.fail(device, f"{device.name} has no primary IP")
+            else:
+                self.log_success(device, f"{device.name} has a primary IP: {device.primary_ip}")
