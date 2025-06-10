7b89c2f82526dd79e4b26bc22597739b47651f2c
prathapshetty-vmo2 <prathap.chandrashetty@virginmediao2.co.uk>
2025-06-10 10:23:25 +0100

    Update netseg_report.py

--- custom_scripts/netseg_report.py
+++ custom_scripts/netseg_report.py
@@ -8,6 +8,6 @@ class DeviceReport(Report):
     def test_primary_ip(self):
         for device in Device.objects.all():
             if device.primary_ip is None:
-                self.info(device, f"{device.name} has no primary IP")
+                self.log_failure(device, f"{device.name} has no primary IP")
             else:
                 self.log_success(device, f"{device.name} has a primary IP: {device.primary_ip}")
