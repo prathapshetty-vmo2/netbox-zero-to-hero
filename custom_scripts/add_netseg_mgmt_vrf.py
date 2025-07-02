4fd33650b6256e1f2509f59b5a8fbe2021c4ccab
prathapshetty-vmo2 <prathap.chandrashetty@virginmediao2.co.uk>
2025-07-02 15:17:54 +0100

    Update add_netseg_mgmt_vrf.py

--- custom_scripts/add_netseg_mgmt_vrf.py
+++ custom_scripts/add_netseg_mgmt_vrf.py
@@ -3,54 +3,13 @@ from extras.scripts import *
 from ipam.models import Prefix
 import os
 import time
-from netbox_netseg_automation.models import ManagementVrf, NetSegSite, SegmentVrf, HubSiteVrf
+from netbox_netseg_automation.models import ManagementVrf, NetSegSite, SegmentVrf, HubSiteVrf, SpokeSiteVrf
 from pynetbox import api
 # import nbox_get_ip
 NETBOX_URL = 'http://192.168.0.105:8000/'
 PARENT_PREFIX_ID = 8
 PREFIX_LENGTH = 29
 DESCRIPTION = "vrf_xxxxx_subnet"
-
-# def get_next_available_prefix(netbox_url, parent_prefix_id, prefix_length, description=None):
-#     """
-#     Get the next available prefix from a parent prefix in NetBox.
-
-#     Args:
-#         netbox_url (str): The NetBox API URL.
-#         parent_prefix_id (int): The ID of the parent prefix.
-#         prefix_length (int): The desired prefix length (e.g., 29).
-#         description (str, optional): Description for the new prefix.
-
-#     Returns:
-#         dict: Details of the newly created prefix, or None if failed.
-#     """
-#     try:
-#         netbox_token = os.getenv("NETBOX_TOKEN")
-#         if not netbox_token:
-#             print("NETBOX_TOKEN environment variable is not set.")
-#             return None
-
-#         # Connect to NetBox
-#         netbox = api(netbox_url, token=netbox_token)
-
-#         # Get the parent prefix
-#         prefix = netbox.ipam.prefixes.get(parent_prefix_id)
-#         if not prefix:
-#             print(f"Parent prefix with ID {parent_prefix_id} not found.")
-#             return None
-
-#         # Create the next available prefix
-#         new_prefix = prefix.available_prefixes.create({
-#             "prefix_length": prefix_length,
-#             "description": description or "auto-assigned"
-#         })
-
-#         return new_prefix
-
-#     except Exception as e:
-#         print(f"Error while retrieving/creating prefix: {e}")
-#         return None
-
 
 def get_and_create_next_prefix(prefix_id, length, description):
     """Reserve the next available prefix via pynetbox, then fetch it in the DB for ORM use."""
@@ -120,14 +79,11 @@ class NewManagementVrfScript(Script):
         # Access selected site
         # selected_site = data['site']
 
-        # Create the new VRF and associate it with the selected site
-        time.sleep(5)
-        vrf_subnet=get_and_create_next_prefix(PARENT_PREFIX_ID, PREFIX_LENGTH, "vrf_to_vdom_subnet")
-        
-        vdom_subnet=get_and_create_next_prefix(PARENT_PREFIX_ID, PREFIX_LENGTH, "inter_vdom_subnet")
-       
-        vdom_loopback=get_and_create_next_prefix(PARENT_PREFIX_ID, PREFIX_LENGTH, "vdom_loopback")
-  
+        # Create the new VRF and associate it with the selected site       
+        # vrf_subnet=get_and_create_next_prefix(PARENT_PREFIX_ID, PREFIX_LENGTH, "vrf_to_vdom_subnet")      
+        # vdom_subnet=get_and_create_next_prefix(PARENT_PREFIX_ID, PREFIX_LENGTH, "inter_vdom_subnet")        
+        # vdom_loopback=get_and_create_next_prefix(PARENT_PREFIX_ID, PREFIX_LENGTH, "vdom_loopback")
+      
         vrf = SegmentVrf.objects.create(
             name=data['vrf_name'],
             vrf_vpn_id=data['vpn_id'],
@@ -151,10 +107,42 @@ class NewManagementVrfScript(Script):
         hub1_vrf_instance = HubSiteVrf.objects.create(
              vrf_name=mgmt_vrf_instance,
              hub_site=data['priority_one_hub_site'],
-             vrf_to_vdom_subnet= vrf_subnet,
-             inter_vdom_subnet=vdom_subnet,
-             vdom_loopback=vdom_loopback,
+             vrf_to_vdom_subnet= get_and_create_next_prefix(PARENT_PREFIX_ID, PREFIX_LENGTH, "vrf_to_vdom_subnet"),
+             inter_vdom_subnet=get_and_create_next_prefix(PARENT_PREFIX_ID, PREFIX_LENGTH, "inter_vdom_subnet")  ,
+             vdom_loopback=get_and_create_next_prefix(PARENT_PREFIX_ID, PREFIX_LENGTH, "vdom_loopback"),
              deployment_status=data['deployment_status']
             )
            
         self.log_success(f"New Mgmt VRF {hub1_vrf_instance.vrf_name} is created for site - {hub1_vrf_instance.hub_site}")
+
+        hub2_vrf_instance = HubSiteVrf.objects.create(
+             vrf_name=mgmt_vrf_instance,
+             hub_site=data['priority_two_hub_site'],
+             vrf_to_vdom_subnet= get_and_create_next_prefix(PARENT_PREFIX_ID, PREFIX_LENGTH, "vrf_to_vdom_subnet"),
+             inter_vdom_subnet=get_and_create_next_prefix(PARENT_PREFIX_ID, PREFIX_LENGTH, "inter_vdom_subnet")  ,
+             vdom_loopback=get_and_create_next_prefix(PARENT_PREFIX_ID, PREFIX_LENGTH, "vdom_loopback"),
+             deployment_status=data['deployment_status']
+            )
+           
+        self.log_success(f"New Mgmt VRF {hub2_vrf_instance.vrf_name} is created for site - {hub2_vrf_instance.hub_site}")
+
+        if data.get('priority_three_hub_site'):
+            hub3_vrf_instance = HubSiteVrf.objects.create(
+             vrf_name=mgmt_vrf_instance,
+             hub_site=data['priority_three_hub_site'],
+             vrf_to_vdom_subnet= get_and_create_next_prefix(PARENT_PREFIX_ID, PREFIX_LENGTH, "vrf_to_vdom_subnet"),
+             inter_vdom_subnet=get_and_create_next_prefix(PARENT_PREFIX_ID, PREFIX_LENGTH, "inter_vdom_subnet")  ,
+             vdom_loopback=get_and_create_next_prefix(PARENT_PREFIX_ID, PREFIX_LENGTH, "vdom_loopback"),
+             deployment_status=data['deployment_status']
+            )
+           
+            self.log_success(f"New Mgmt VRF {hub3_vrf_instance.vrf_name} is created for site - {hub3_vrf_instance.hub_site}")
+
+        if data.get('vrf_spoke_sites'):        
+           for spoke_vrf in data['vrf_spoke_sites']:
+              spoke_vrf_inst = SpokeSiteVrf.objects.create(
+                  name = spoke_vrf,
+                  vrf_name=mgmt_vrf_instance,
+ 
+              )
+              self.log_success(f"New Mgmt VRF {spoke_vrf_inst.name} is created for site - {spoke_vrf.name}")    
